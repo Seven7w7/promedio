4643faff8f0b0f7a0b278d5fd1e923ee442c17aa
���Steven <villegas132004@gmail.com>
2023-08-14 06:38:15 -0500

    Commit inicial[tercero

---
+++ promedio.js
@@ -0,0 +1,7 @@
+let num1=Number(prompt("Ingrese el primer número"));
+let num2=Number(prompt("Ingrese el segundo número"));
+
+let resultado=num1+num2
+let resultadofinal=resultado/2
+
+alert(resultadofinal);
